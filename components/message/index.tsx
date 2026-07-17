34e266f2eb5ecbb2adbe02f6b1e36ee8eee4abfc
Victor Cruz <victtorrhugo100@gmail.com>
2026-07-17 15:07:42 -0300

    feat: render message based on type

--- components/message/index.tsx
+++ components/message/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
 import Bubble from "../ui/bubble";
@@ -11,6 +12,39 @@ type MessageProps = {
 export function Message(props: MessageProps) {
 
   const { message } = props;
+
+  if(message.type === "image"){
+    return (
+      <MessageScroller.Item
+        key={message.id}
+        messageId={message.id}
+        scrollAnchor={message.sender === "myself"}
+        className="w-full"
+      >
+        <Bubble.Root className="w-full max-w-full" align={message.sender === "myself" ? "end" : "start"} variant={message.sender === "myself" ? "default" : "tinted"}>
+          <Bubble.Content className="max-w-1/2">
+            <img src={message.content} alt="Mensagem de imagem" className="max-w-full h-auto" />
+          </Bubble.Content>
+        </Bubble.Root>
+      </MessageScroller.Item>
+    )
+  }
+  if(message.type === "audio"){
+    return (
+      <MessageScroller.Item
+        key={message.id}
+        messageId={message.id}
+        scrollAnchor={message.sender === "myself"}
+        className="w-full"
+      >
+        <Bubble.Root className="w-full max-w-full" align={message.sender === "myself" ? "end" : "start"} variant={message.sender === "myself" ? "default" : "tinted"}>
+          <Bubble.Content className="max-w-1/2">
+            <audio src={message.content} controls />
+          </Bubble.Content>
+        </Bubble.Root>
+      </MessageScroller.Item>
+    )
+  }
   return (
     <>
       <MessageScroller.Item
